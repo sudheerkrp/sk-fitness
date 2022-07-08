ebfe4fc3dacd81d8c5e431f5f2607f58ca5a4712
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-07-08 19:40:34 +0530

    Basic File & Folder Structure Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>SK Gym</div>
+    <Router>
+      <Box width ="400px" sx={{width: {xl: '1488px'}}} m="auto">
+        <Navbar></Navbar>
+          <Routes>
+            <Route exact path='/' element={<Home></Home>}></Route>
+            <Route exact path='/exercise/:id' element={<ExerciseDetail></ExerciseDetail>}></Route>
+          </Routes>
+        <Footer></Footer>
+      </Box>
+    </Router>
   );
 }
 
