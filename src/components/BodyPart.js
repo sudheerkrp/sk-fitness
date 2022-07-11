435c4c7dbd42b3eee58169ca27010a4bf7615d06
Sudheer Kumar Prajapat <sudheerkumar8929@gmail.com>
2022-07-11 22:56:39 +0530

    BodyPart Component Almost Completed

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Stack, Typography} from '@mui/material';
+import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ({item, bodyPart, setBodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack type="button" alignItems='center' justifyContent='center' className='body-part-card' sx ={{borderTop: (bodyPart===item)?('4px solid #ff2625'):(''), backgroundColor:'#fff', borderBottomLeftRadius:'20px', width:'270px', height:'280px', cursor:'pointer', gap:'47px'}} onClick={()=>{setBodyPart(item); window.scrollTo({top:1800, left: 100, behavior:'smooth'})}}>
+        <img src={Icon} alt="Dumbbell" style={{height:'40px', width:'40px'}}/>
+        <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
+    </Stack>
   );
 }
 
